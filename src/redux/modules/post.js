375d9ceb1d1624e2bfc1ac2daa1b3a9bdafd9235
nujeyh <dza456@naver.com>
2022-06-18 18:16:52 +0900

    액션 추가

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -3,10 +3,16 @@ import { produce } from "immer";
 import axios from "axios";
 import { apis } from "../../shared/api";
 // Action
+const GET_POST_LIST = "GET_POST_LIST";
 const ADD_POST = "ADD_POST";
+const EDIT_POST = "EDIT_POST";
+const DELETE_POST = "DELETE_POST";
 
 // Action Creator
-const addPost = createAction(ADD_POST, (postObj) => ({ postObj }));
+const getPostList = createAction(GET_POST_LIST, (postList) => ({ postList }));
+const addPost = createAction(ADD_POST, (post) => ({ post }));
+const editPost = createAction(EDIT_POST, (post) => ({ post }));
+const deletePost = createAction(DELETE_POST, (postId) => ({ postId }));
 
 // Initial State
 const initialState = {
@@ -29,6 +35,21 @@ const initialState = {
 };
 
 // Middleware
+
+// 전체 게시물 받아오기
+export const getPostListDB = () => {
+  return async function (dispatch) {
+    apis
+      .loadPostList()
+      .then((response) => {
+        dispatch(getPostList(response));
+      })
+      .catch((error) => {
+        window.alert("게시물을 불러오는 중에 오류가 발생했습니다.");
+        console.log(error);
+      });
+  };
+};
 
 // 게시물 업로드
 export const addPostDB = (formData) => {
@@ -53,12 +74,49 @@ export const addPostDB = (formData) => {
   };
 };
 
+// 게시물 삭제
+export const deletePostDB = (id) => {
+  return async function (dispatch) {
+    try {
+      // await apis.deletePost(id);
+
+      dispatch(deletePost(id));
+    } catch (error) {
+      alert("댓글 삭제 중에 오류가 발생했습니다.");
+      console.log(error);
+    }
+  };
+};
+
 // Reducer
 export default handleActions(
   {
+    [GET_POST_LIST]: (state, { payload }) =>
+      produce(state, (draft) => {
+        draft.postList = payload;
+      }),
     [ADD_POST]: (state, { payload }) =>
       produce(state, (draft) => {
         draft.postList.unshift(payload.postObj);
+      }),
+    [EDIT_POST]: (state, { payload }) =>
+      produce(state, (draft) => {
+        draft.postList = state.postList.map((post) => {
+          // console.log(post.id, payload.id, payload);
+          if (post.id === payload.id) {
+            return {
+              ...post,
+              imageFile: payload.imageFile,
+              content: payload.content,
+            };
+          } else {
+            return post;
+          }
+        });
+      }),
+    [DELETE_POST]: (state, { payload }) =>
+      produce(state, (draft) => {
+        draft.postList = draft.postList.filter((post) => post.id !== payload);
       }),
   },
   initialState
