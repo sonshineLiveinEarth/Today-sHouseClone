75b33c69f3808bcba95d0d17315c288f959fb205
nujeyh <dza456@naver.com>
2022-06-18 16:13:10 +0900

    초기 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+
+import Main from "./pages/Main";
+import PostNewEdit from "./pages/PostNewEdit";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/contents/new" element={<PostNewEdit />} />
+      </Routes>
+    </>
+  );
 }
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 export default App;
