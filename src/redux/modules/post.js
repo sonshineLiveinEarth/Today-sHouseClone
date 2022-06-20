212498d9f76893ddc21d994f2ea94ea78148b055
nujeyh <dza456@naver.com>
2022-06-20 09:23:16 +0900

    edit 추가

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -12,7 +12,7 @@ const DELETE_POST = "DELETE_POST";
 const getPostList = createAction(GET_POST_LIST, (postList) => ({ postList }));
 const addPost = createAction(ADD_POST, (post) => ({ post }));
 const editPost = createAction(EDIT_POST, (post) => ({ post }));
-const deletePost = createAction(DELETE_POST, (postId) => ({ postId }));
+const deletePost = createAction(DELETE_POST, (id) => ({ id }));
 
 // Initial State
 const initialState = {
@@ -29,7 +29,8 @@ const initialState = {
       userNickname: "nickname",
       imageFile:
         "https://www.instandngs4p.eu/wp-content/themes/fox/images/placeholder.jpg",
-      content: "content2",
+      content:
+        "비오려고 하루종일 후덥지근 해요. 몸이 찌뿌둥하고 깔아져서 하루종일 침대밖을 못벗어났어요😔 계속 이러고 있어선 안되겠어서 잘 밤에 부지런을 떨어봤는데요. 간만에 구조도 바꾸고 이불도 교체했답니다 :) 하루종일 후덥지근 눅눅했는데 산뜻하고 쾌적해서 모모도 좋아하네요🐱💙 구조바꾸는게 힘들긴 하지만 하고나면 뿌듯하더라고요. 오늘은 분리형 구조로🤍",
     },
   ],
 };
@@ -52,21 +53,24 @@ export const getPostListDB = () => {
 };
 
 // 게시물 업로드
-export const addPostDB = (formData) => {
+export const addPostDB = (id, formData) => {
   return async function (dispatch) {
     try {
-      // await apis.addPost(formData);
-
-      const postObj = {};
-      for (let key of formData.keys()) {
-        // console.log({ [key]: formData.get(key) });
-        postObj[key] = formData.get(key);
+      if (id) {
+        // await apis.editPost(id,formData)
+      } else {
+        // await apis.addPost(formData);
       }
-      postObj.imageFile =
-        "https://www.instandngs4p.eu/wp-content/themes/fox/images/placeholder.jpg";
-      postObj.userNickname = "nickname";
-      //   console.log(postObj);
-      dispatch(addPost(postObj));
+
+      const post = {};
+      for (let key of formData.keys()) {
+        // console.log("request :", { [key]: formData.get(key) });
+        post[key] = formData.get(key);
+      }
+      post.imageFile = "/images/Logo.png";
+      post.userNickname = "nickname";
+      // console.log(post);
+      // dispatch(addPost(post));
     } catch (error) {
       window.alert("게시물 등록 중에 오류가 발생했습니다.");
       console.log(error);
@@ -97,7 +101,8 @@ export default handleActions(
       }),
     [ADD_POST]: (state, { payload }) =>
       produce(state, (draft) => {
-        draft.postList.unshift(payload.postObj);
+        console.log(payload);
+        draft.postList.unshift(payload.post);
       }),
     [EDIT_POST]: (state, { payload }) =>
       produce(state, (draft) => {
@@ -116,7 +121,9 @@ export default handleActions(
       }),
     [DELETE_POST]: (state, { payload }) =>
       produce(state, (draft) => {
-        draft.postList = draft.postList.filter((post) => post.id !== payload);
+        draft.postList = draft.postList.filter(
+          (post) => post.id !== payload.id
+        );
       }),
   },
   initialState
